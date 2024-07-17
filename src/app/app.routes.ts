b5a50a6f1caa0bb27c3e5ad315d068b9044bb95d
Revivedaniel <mlg.danielstark@gmail.com>
2024-07-17 09:15:55 -0700

    Created the shopping and recipes componenets and connected them through routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', component: RecipesComponent},
+    {path: 'shopping', component: ShoppingComponent}
+];
