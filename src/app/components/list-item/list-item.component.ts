6e86def7130221a38efbca40708dcf40adc5bb37
Revivedaniel <mlg.danielstark@gmail.com>
2024-07-28 10:46:34 -0700

    Updated the styles on the shopping list.

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { ShoppingListItem } from '../../models/shoppingListItem.model';
 import { ShoppingListService } from '../../services/shopping-list.service';
 import { MatDialog } from '@angular/material/dialog';
@@ -10,7 +9,7 @@ import { ShoppingListFormComponent } from '../shopping-list-form/shopping-list-f
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.scss',
 })
